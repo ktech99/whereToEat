2a78c027677e6f975015786dcbd22b8f13b6619a
Kartik Arora <kartikar@uw.edu>
2018-12-10 15:35:56 -0800

    Initial project commit

---
+++ src/main/java/com/brownHouse/whereToEat/WhereToEatApplication.java
@@ -0,0 +1,12 @@
+package com.brownHouse.whereToEat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereToEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereToEatApplication.class, args);
+	}
+}
