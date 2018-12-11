7d8b136d9386c26e28c7b334cd4ddefce2d61777
Kartik Arora <kartikar@uw.edu>
2018-12-10 23:31:03 -0800

    basic landing page UI with 2 buttons

--- src/main/resources/src/App.js
+++ src/main/resources/src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Update this file.
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={"blackBack"}>
+                <div className='navbar'>
+                    <div className='logo'>Where to Eat ?</div>
+                    <div className='seal'/>
+                    <div className={"buttons"}>
+                        <button className={"button-submit"}>Create Form</button>
+                        <br/>
+                        <button className={"button-submit"}>Enter code</button>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
