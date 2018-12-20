d143d9ea1fa167e8035259f96bcd9707ca720667
Kartik Arora <kartikar@uw.edu>
2018-12-20 17:25:44 -0600

    add nearbyResaturants api

--- src/main/java/com/brownHouse/whereToEat/restaurantMap.java
+++ src/main/java/com/brownHouse/whereToEat/restaurantMap.java
@@ -1,10 +1,14 @@
 package com.brownHouse.whereToEat;
 
 
+import com.sun.tools.javac.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Immutable class which is not an ADT
@@ -31,4 +35,22 @@ public class restaurantMap {
         // TODO Add hash as key, latLong as value to DB
         return hash;
     }
+
+    /**
+     * Returns a set of restaurants within 2 miles
+     * @param code hashcode of form
+     * @return List of restaurants within 2 miles
+     * null if code is not in DB
+     */
+    @GetMapping("/getNearbyRestaurants")
+    public Set<Restaurant> getNearbyRestaurants(@RequestParam(value = "code") int code){
+        // Set of restaurants to be returned
+        Set<Restaurant> restaurants = new HashSet<Restaurant>();
+        // Set<Restaurant> all restaurants= get from DB
+        // Location location = //Get location of code from DB
+        // add 40 restaurants within 2 miles
+        return restaurants;
+    }
+
+    //Todo figure out how to get likes and dislikes
 }
