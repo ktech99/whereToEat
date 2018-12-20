7c164b2959b3cdec576d42486607e73b303ff430
Kartik Arora <kartikar@uw.edu>
2018-12-20 16:55:51 -0600

    make API callable

--- src/main/java/com/brownHouse/whereToEat/restaurantMap.java
+++ src/main/java/com/brownHouse/whereToEat/restaurantMap.java
@@ -1,17 +1,34 @@
 package com.brownHouse.whereToEat;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Immutable class which is not an ADT
  * A restaurant map is used as access point to the API of the program
  */
+@RestController
+@CrossOrigin("http://localhost:3000")
 public class restaurantMap {
-// todo
 
-    public int getCode(double latitude, double longitude){
+    /**
+     * Gets hashcode for the call, and adds latitude, longitude to DB with
+     * hashcode as the key.
+     * This is used to create a different code for every time a person makes a
+     * new form to share
+     * @param latitude Latitude of the person
+     * @param longitude Longitude of the person
+     * @return Hashcode for the given API call
+     */
+    @GetMapping("/getCode")
+    public int getCode(@RequestParam(value = "latitude") double latitude,@RequestParam(value = "longitude") double longitude){
         Location personLocation = new Location(latitude, longitude);
         Long currTime = System.nanoTime();
         int hash = currTime.hashCode() + personLocation.hashCode();
-        // Add hash as key, latLong as value to DB
+        // TODO Add hash as key, latLong as value to DB
         return hash;
     }
 }
