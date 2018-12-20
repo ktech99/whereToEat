a37a5b8fdc38bba8fffc62b79fec99b9efeeea8b
Kartik Arora <kartikar@uw.edu>
2018-12-20 16:37:20 -0600

    add get Code function to API

--- src/main/java/com/brownHouse/whereToEat/restaurantMap.java
+++ src/main/java/com/brownHouse/whereToEat/restaurantMap.java
@@ -1,5 +1,17 @@
 package com.brownHouse.whereToEat;
 
+/**
+ * Immutable class which is not an ADT
+ * A restaurant map is used as access point to the API of the program
+ */
 public class restaurantMap {
+// todo
 
+    public int getCode(double latitude, double longitude){
+        Location personLocation = new Location(latitude, longitude);
+        Long currTime = System.nanoTime();
+        int hash = currTime.hashCode() + personLocation.hashCode();
+        // Add hash as key, latLong as value to DB
+        return hash;
+    }
 }
